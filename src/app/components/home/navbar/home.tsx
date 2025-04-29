ce2efb5cdd8e8e53ab9ee241ef9358ba9c7e5cb5
Laraib Tabbassum <161297553+laraib28@users.noreply.github.com>
2025-04-29 13:58:38 +0500

    update code

--- src/app/components/home/navbar/home.tsx
+++ src/app/components/home/navbar/home.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Hero from '../hero/page';
-import About from '../../../about/page';
-import Services from '../../../services/page';
-import Project from '../../../projects/page';
-import Skills from '../../../skills/page';
+// import React from 'react';
+// import Hero from '../hero/page';
+// import About from '../../../about/page';
+// import Services from '../../../services/page';
+// import Project from '../../../projects/page';
+// import Skills from '../../../skills/page';
 // import Blog from '../blog/blog';
-import Contact from '../../../contact/page'
+// import Contact from '../../../contact/page'
 
 
 const Home = () => {
